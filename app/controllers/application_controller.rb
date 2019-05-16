dc9e4016100a2d606a0b1d7aa5d5e12c02737b93
nachika <nachika0628@mercari.com>
2019-05-16 22:07:53 +0900

    momo update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-     render html:"this is a test app"
+     render html:"お腰に付けたきび団子ひとつ私にgive me plz"
   end
   def goodbye
      render html:"goodbye, world!"
