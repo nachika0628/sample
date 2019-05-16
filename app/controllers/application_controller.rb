ab077f3db42008b12ba16e5a086685c1723d82a3
nachika <nachika0628@mercari.com>
2019-05-16 22:03:16 +0900

    change details

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-     render html:"Hello, World!"
+     render html:"this is a test app"
   end
   def goodbye
      render html:"goodbye, world!"
